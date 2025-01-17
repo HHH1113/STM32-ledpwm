3682b140b8530146c655e0cac2fa49c9bd27524b
unknown <sun67000@naver.com>
2025-01-17 15:12:05 +0900

    LED의 동작 코드 수정

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -19,9 +19,15 @@ void setDuty(uint8_t duty) {
 void app() {
 	// 타이머 장치 시작
 	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
 	while(1) {
-		static int angle = 0;
-		angle ++;
+		static int angleR = 0;
+		static int angleG = 120;
+		static int angleB = 240;
+		angleR ++;
+		angleG ++;
+		angleB ++;
 		angle %= 360;
 		uint8_t valueSin = sin(angle * M_PI / 180) * 50 +49;
 		setDuty(valueSin);
